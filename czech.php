ebaf484aaf6e3bd6f39eeec0a23793889286a38e
Alicja Łysień <alicja.lysien@gmail.com>
2024-03-11 08:53:16 +0100

    unused file removed

--- czech.php
+++ czech.php
@@ -3,4 +3,3 @@
 $czech_months = array('Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec');
 
 $czech_week = array('po', 'út', 'st', 'čt', 'pá', 'so', 'ne');
-
--- czech_months.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$czech_months = array('Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec');
